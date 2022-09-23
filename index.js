7df1fb83b54538985e6d634076eb1bae3c0aed96
Andrew <afellman84@gmail.com>
2022-09-23 10:38:24 -0400

    commit tree 0c11b25e75ae2dc695092446220111a0e8e13cb5

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (parameter1 = 2000, parameter2 = 100) => {
+    console.log(`divide ${parameter1}`);
+    console.log(`divide ${parameter2}`);
+    return parameter1 / parameter2;
+}
+
+
+const square = (parameter1) => parameter1 * parameter1;
+square(2)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(3,4);
